44aa95843feaff65134a69a7a501fec89f3b1d95
Daukaevt <mupbam1@gmail.com>
2026-01-04 17:32:26 +0300

    test bugs fixed

--- b9-gateway/src/test/java/com/wixsite/mupbam1/b9_gateway/B9GatewayApplicationTests.java
+++ b9-gateway/src/test/java/com/wixsite/mupbam1/b9_gateway/B9GatewayApplicationTests.java
@@ -2,8 +2,12 @@ package com.wixsite.mupbam1.b9_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {"eureka.client.enabled=false"})
 class B9GatewayApplicationTests {
 
 	@Test
--- b9-hello-world-service/src/test/java/com/wixsite/mupbam1/b_hello_world/B9HelloWorldServiceApplicationTests.java
+++ b9-hello-world-service/src/test/java/com/wixsite/mupbam1/b_hello_world/B9HelloWorldServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.wixsite.mupbam1.b_hello_world;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {"eureka.client.enabled=false"})
 class B9HelloWorldServiceApplicationTests {
 
 	@Test
